1e21bd8597caca38428b5b31ec059be2f3609277
carpelli <92866289+carpelli@users.noreply.github.com>
2025-07-27 12:36:13 +0200

    update

--- fish.nix
+++ fish.nix
@@ -9,15 +9,24 @@
         src = pkgs.fishPlugins.fzf-fish.src;
       }
       {
-        name = "hydro";
-        src = pkgs.fishPlugins.hydro.src;
+        name = "pure";
+        src = pkgs.fishPlugins.pure.src;
       }
       {
         name = "autopair";
 	src = pkgs.fishPlugins.autopair.src;
       }
+      {
+        name = "done";
+	src = pkgs.fishPlugins.done.src;
+      }
+      {
+        name = "async-prompt";
+	src = pkgs.fishPlugins.async-prompt.src;
+      }
     ];
     shellInit = ''
+      set -g async_prompt_functions _pure_prompt_git
       source ~/.nix-profile/etc/profile.d/nix.fish
     '';
   };
--- home.nix
+++ home.nix
@@ -24,6 +24,7 @@
     pkgs.bat
     pkgs.fd
     pkgs.mosh
+    pkgs.yazi
 
     pkgs.git
     pkgs.ffmpeg
