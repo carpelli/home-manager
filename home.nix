f3c97b9ae4da5193c477124051be686ee706d3a6
carpelli <92866289+carpelli@users.noreply.github.com>
2025-08-26 18:30:40 +0200

    add editor

--- home.nix
+++ home.nix
@@ -88,7 +88,7 @@
   #  /etc/profiles/per-user/otis/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "hx";
   };
 
   # Let Home Manager install and manage itself.
